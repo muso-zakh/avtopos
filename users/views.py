b371c62a4098f9ac3bafc17d53e6f6b333a4381a
muso-zakh <ccvaf0803@gmail.com>
2025-09-01 11:38:20 +0500

    permissions

--- users/views.py
+++ users/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
 from .serializers import UserSerializer, UserCreateSerializer
+from .permissions import IsStaffUser
 
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsStaffUser]
 
 
     def get_serializer_class(self):
